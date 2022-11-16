33a92e560d4330352e5e940ce8546c7124146f22
carlos pana <panacarlosramon@gmail.com>
2022-11-16 11:50:58 -0300

    ghfjghjghj

--- src/redux/action.js
+++ src/redux/action.js
@@ -8,11 +8,8 @@ export function getDATA(id_tuit) {
     return async function (dispatch) {
 
         try {
-            const tweetEndPoint = `https://api.twitter.com/2/tweets/${id_tuit}?media.fields=preview_image_url,variants&expansions=attachments.media_keys`
-            const token = 'AAAAAAAAAAAAAAAAAAAAAM95jQEAAAAA2FDZu8tmMzKzdDDJHmLOdVeS0pw%3DibFCIuD5drVDqCIHZKuZj67LAGpSYxEYkFFjvdsJdaAPzNgz90'
-            console.log(tweetEndPoint);
-            console.log(token);
-            const json = await axios.get()
+            
+            const json = await axios.get("https://tuiter-down-production.up.railway.app/tweetUrlVideos")
 
             console.log(json);
 
