9c2e909aad97bbb8b4b9cf213d6e2b28a55cd08b
carlos pana <panacarlosramon@gmail.com>
2022-11-16 11:44:08 -0300

    abbbbbbbbbbbbb

--- api/index.js
+++ api/index.js
@@ -16,7 +16,7 @@ server.get('/tweetUrlVideos', async (req, res) => {
     // console.log(token);
     // const { id_tuit } = req.body;
     const id_tuit = "1569437835027181569";
-    console.log(id_tuit,'este es el body');
+    console.log(id_tuit, 'este es el body');
     const tweetEndPoint = `https://api.twitter.com/2/tweets/${id_tuit}?media.fields=preview_image_url,variants&expansions=attachments.media_keys`
 
 
@@ -37,7 +37,7 @@ server.get('/tweetUrlVideos', async (req, res) => {
     })
 
     console.log(tuitDataMap);
-    res.json('prueba anda')
+    res.status(200).json(tuitDataMap)
 })
 
 server.listen(PORT || 3001, () => {
