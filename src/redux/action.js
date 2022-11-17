f14812975576746ac6fca7ed29c455d6b5ecd435
carlos pana <panacarlosramon@gmail.com>
2022-11-17 12:41:50 -0300

    fix cors localhost

--- src/redux/action.js
+++ src/redux/action.js
@@ -9,8 +9,8 @@ export function getDATA(id) {
 
         try {
             // console.log(id, 'aca el id en action');
-            // const json = await axios.get(`https://tuiter-down-production.up.railway.app/tweetUrlVideos?id=${id}`)
-            const json = await axios.get(`http://localhost:3001/tweetUrlVideos?id=${id}`)
+            const json = await axios.get(`https://tuiter-down-production.up.railway.app/tweetUrlVideos?id=${id}`)
+            // const json = await axios.get(`http://localhost:3001/tweetUrlVideos?id=${id}`)
 
 
 
